d8aec131befac9d694db339b3e34aff287f67eb6
Siskiz <markevi4.Alex@yandex.ru>
2023-02-02 15:30:56 +0300

    Add project

---
+++ src/main/java/com/example/springbootrest/dao/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.example.springbootrest.dao;
+
+
+
+import com.example.springbootrest.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
